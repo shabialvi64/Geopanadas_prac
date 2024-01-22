c0bc5c822d88376f64a32d2a59c99bffceae8d95
Shoaib Alvi <130247936+shabialvi64@users.noreply.github.com>
2024-01-22 11:20:50 +0500

    Add files via upload

---
+++ Geopandas_prac.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import geopandas as gpd
+#import fiona
+
+st.title("Geopandas Streamlit Web App")
+
+# Load data
+shapefile= gpd.read_file('D:/Branch_Locator/Pakistan Boundary/Pakistan.shp')
+# url = "https://raw.githubusercontent.com/mhaffner/data/master/states.geojson"
+# states = gpd.read_file(url)
+
+# Display data
+st.write(shapefile)
+#st.write(states)
